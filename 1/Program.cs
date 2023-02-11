2f3bff335dbdc75b6c825c4a36743baae610b8a4
Aleksandr_Wosmerik <shurik1777@gmail.com>
2023-02-11 07:56:57 +0300

    first home work done!

--- 1/Program.cs
+++ 1/Program.cs
@@ -3,4 +3,18 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+int a = -9;
+int b = -3;
 
+int max = a;
+int min = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (a < min ) min = a;
+if (b < min ) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
