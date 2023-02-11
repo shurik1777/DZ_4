1efd95da1f0358a43a7ea8f4ed7a6a29185efe39
Aleksandr_Wosmerik <shurik1777@gmail.com>
2023-02-11 08:04:42 +0300

    second home work done!

--- 2/Program.cs
+++ 2/Program.cs
@@ -3,3 +3,15 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+int a = 22;
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
