648200959a2fdca721889a9b820fae43efc03d20
Aleksandr_Wosmerik <shurik1777@gmail.com>
2023-02-12 09:24:05 +0300

    fourth project done!

--- 4/Program.cs
+++ 4/Program.cs
@@ -2,3 +2,18 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// int count = 1; // четные от 1
+// while (count < N) 
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.WriteLine(""+count+"");
+//     }
+//     count++;
+// }
+for (int i = 2; i < N; i += 2)
+{
+    Console.WriteLine(" " + i + " ");
+}
